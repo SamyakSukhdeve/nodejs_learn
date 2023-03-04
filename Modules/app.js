be1b245ebb56837f47e0a89e03c97ea4e6467849
Samyak Sukhdeve <samyaksukhadeve12@gmail.com>
2023-03-04 12:29:03 +0530

    learned about how modules work

---
+++ Modules/app.js
@@ -0,0 +1,5 @@
+const { add, sub } = require("./logic");
+const { number1, number2 } = require("./numbers");
+
+add(number1, number2);
+sub(number1, number2);
---
+++ Modules/numbers.js
@@ -0,0 +1,4 @@
+const number1 = 15;
+const number2 = 20;
+
+module.exports = { number1, number2 };
